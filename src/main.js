9a9812b85f73ec03131a199434fb406a4c5d3c98
LRauscher <134446069+LRauscher@users.noreply.github.com>
2024-07-02 10:12:15 +0200

    Changes

--- src/main.js
+++ src/main.js
@@ -31,6 +31,7 @@ const returnYesBtn = document.getElementById('returnYes');
 const FocusTextEl = document.getElementById('whyEl');
 const counterDivs = document.querySelectorAll('.counter');
 const completeFocusEl = document.getElementById('focusTimeDisplay');
+const totalFocusEl = document.getElementById('totalTimeDisplay');
 
 if (!timerDisplay || !btnDivEl || focusButtons.length === 0) {
   console.error("One or more key elements not found in the DOM.");
@@ -46,7 +47,7 @@ let durationValue = durationInputEl.value;
 const quitText = 'i want to give up!';
 let progressWidth = 0;
 let totalFocusTime = 0;
- 
+let allTimeFocus = 0;
 
 // Function to make the 20-second timer
 function counter(counterDivs) {
@@ -110,6 +111,7 @@ function startTimer(durationValue) {
   timerInterval = setInterval(() => {
     timeRemaining--; // Decrease the time remaining
     totalFocusTime++;
+    allTimeFocus++;
     updateDisplay(initialDuration); // Update the timer display
     if (randomTimes.includes(timeRemaining)) {
       triggerRandomEvent(); // Trigger your random event here
@@ -136,6 +138,7 @@ function saveTotalFocusTime() {
   const focusData = {
     date: new Date().toDateString(),
     time: totalFocusTime,
+    allTime: allTimeFocus,
   };
   localStorage.setItem('totalFocusTime', JSON.stringify(focusData));
 }
@@ -143,13 +146,14 @@ function saveTotalFocusTime() {
 function loadTotalFocusTime() {
   const savedData = localStorage.getItem('totalFocusTime');
   if (savedData) {
-    const { date, time } = JSON.parse(savedData);
+    const { date, time, allTime } = JSON.parse(savedData);
     const today = new Date().toDateString();
     if (date === today) {
       totalFocusTime = time;
     } else {
       totalFocusTime = 0;
     }
+    allTimeFocus = allTime || 0;
   }
 }
 
@@ -159,6 +163,14 @@ function displayTotalFocusTime() {
   const seconds = totalFocusTime % 60;
   const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
   completeFocusEl.textContent = `${formattedTime}`;
+}
+
+function displayAllFocusTime() {
+  const hours = Math.floor(allTimeFocus / 3600);
+  const minutes = Math.floor((allTimeFocus % 3600) / 60);
+  const seconds = allTimeFocus % 60;
+  const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  totalFocusEl.textContent = `${formattedTime}`;
 }
 
 // Function to update the timer display
@@ -172,6 +184,7 @@ function updateDisplay(initialDuration) {
   progressWidth = progressPercentage;
 
   displayTotalFocusTime();
+  displayAllFocusTime();
 }
 
 function resetTimer() {
@@ -217,6 +230,7 @@ function resumeTimer() {
     timerInterval = setInterval(() => {
       timeRemaining--; // Decrease the time remaining
       totalFocusTime++;
+      allTimeFocus++;
       updateDisplay(initialDuration); // Update the timer display
       if (randomTimes.includes(timeRemaining)) {
         triggerRandomEvent(); // Trigger your random event here
@@ -292,6 +306,7 @@ function quitTimer() {
   console.log('quitting');
   stopTimer();
   resetTimer();
+  saveTotalFocusTime();
   quitDiv.style.display = 'none';
   timerContainer.style.display = 'none';
   timerDivEl.style.display = 'none';
@@ -317,6 +332,7 @@ function getQuitValue() {
       let quitValueEl = quitInput.value;
       if (quitText === quitValueEl) {
         quitTimer();
+        stopCounter();
       } else {
         quitDiv.style.display = 'none';
         timerContainer.style.display = 'block';
@@ -361,6 +377,26 @@ let msgArray = [
   'A break now, better focus later'
 ];
 
+function getBreakTime(duration) {
+ 
+  switch (duration) {
+    case 3600: // 1 Stunde
+      breakTime = 300; // 10 Minuten Pause
+      break;
+    case 1800: // 2 Stunden
+      breakTime = 180; // 20 Minuten Pause
+      break;
+    case 900: // 3 Stunden
+      breakTime = 120; // 30 Minuten Pause
+      break;
+    default: // Für andere Dauer
+      breakTime = 60; // Standardmäßig 5 Minuten Pause
+      break;
+  }
+
+  return breakTime;
+}
+
 // Function to handle taking a break
 function breakTimer() {
   // Hide timer elements and display break elements
@@ -368,7 +404,7 @@ function breakTimer() {
   timerDivEl.style.display = 'none';
   timerContainer.style.display = 'none';
 
-  breakTime = 120; // Break time in seconds
+  getBreakTime();
   breakDisplay(); // Initial break display
   activateLights(6000, breakLights);
 
@@ -533,7 +569,7 @@ const gamesContainer = document.getElementById('gamesContainer');
 const games = [
   {
     name: 'Wordle',
-    explanation: "Guess the 5-letter word within six attempts. Each guess must be a valid 5-letter word. The color of the tiles will change to show how close your guess was to the word.",
+    explanation: "Guess the searched word within six attempts. The color of the tiles will change to show how close your guess was to the word. Green means right character at the right place. Yellow means right character but wrong position",
     play: (callback) => wordle(gamesContainer, callback)
   },
   {
@@ -543,13 +579,13 @@ const games = [
   },
   {
     name: 'Catch the Objects',
-    explanation: 'Control a player at the bottom of the screen using the arrow keys to catch falling objects, aiming to increase your score by catching as many as possible while avoiding the black obstacles.',
+    explanation: 'Control a player at the bottom of the screen using the arrow keys to catch falling objects. Increase your score by catching as many objects as possible while avoiding the black obstacles.',
     play: (callback) => catchObjects(gamesContainer, callback)
   },
   {
     name: 'Speedtest',
-    explanation: 'Type the displayed words as quickly as you can within 30 seconds to see how many you can get right!',
-    play: (callback => speedtestGame(gamesContainer, callback))
+    explanation: 'Type the displayed words as quickly as you can within 60 seconds. Press the spacebar to submit your word. Correct letters will be highlighted in green and incorrect letters in red. Try to get as many words right as possible!',
+    play: (callback) => speedtestGame(gamesContainer, callback)
   },
 ];
 
@@ -652,7 +688,7 @@ function displayRandomGame() {
         // Handle game finished logic here
         gamesContainer.innerHTML = `
         <div id="afterGame">
-          <p id="textEl">The game has finished. Do you want to play another? Or do you want to go back to focus?</p>
+          <p id="textEl">The task has finished. Do you want to play another? Or do you want to go back to focus?</p>
           <div afterGameBtnEl>
             <button id="playAnotherBtn">Play another</button>
             <button id="declineAnotherPlayBtn">Return to focus</button>
@@ -686,4 +722,5 @@ function displayRandomGame() {
 window.addEventListener('load', () => {
   loadTotalFocusTime();
   displayTotalFocusTime();
+  displayAllFocusTime();
 });
